3ad6a689ff36b30ceb2870d7244f281f8ccd3ce1
DamThanhLam <ldamthanh@gmail.com>
2025-01-15 10:02:59 +0700

    initial project

---
+++ src/main/java/fit/iuh/edu/com/LearningManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package fit.iuh.edu.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningManagementSystemApplication.class, args);
+    }
+
+}
