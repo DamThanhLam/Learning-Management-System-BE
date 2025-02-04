fd5c6a30236fcf67f7afb796a1d3af18d2598221
DamThanhLam <121682538+DamThanhLam@users.noreply.github.com>
2025-02-04 23:02:47 +0700

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,4 +2,5 @@
 set -e
 cd "$(dirname "$0")/.."
 docker build -t image_lms_course_service:latest course_service/
-docker run -d -p 8080:8080 --name container_lms_course_service container_lms_course_service:latest
+docker run -d -p 8080:8080 --name container_lms_course_service image_lms_course_service:latest
+
