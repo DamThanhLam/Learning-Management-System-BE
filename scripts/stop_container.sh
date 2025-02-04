24d7abc567b9ce342c917f675a19dba2e992b326
DamThanhLam <121682538+DamThanhLam@users.noreply.github.com>
2025-02-04 09:38:05 +0700

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,4 +1,4 @@
 set -e
 #docker stop container_lms_course_service
 docker kill $(docker ps -q)
-docker rm -f container_lms_course_service
+docker ps -a | grep container_lms_course_service && docker rm container_lms_course_service
