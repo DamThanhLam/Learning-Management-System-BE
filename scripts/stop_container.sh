a2a9f18a6e06bdd7d1e46c8b8897f754e62c7ca7
DamThanhLam <121682538+DamThanhLam@users.noreply.github.com>
2025-02-04 22:04:11 +0700

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,13 +1,3 @@
 set -e
 #docker stop container_lms_course_service
-# Lấy danh sách container đang chạy
-CONTAINERS=$(docker ps -q)
-
-# Kiểm tra nếu có container thì mới kill
-if [ -n "$CONTAINERS" ]; then
-    docker kill $CONTAINERS
-    echo "Killed containers: $CONTAINERS"
-else
-    echo "No running containers to kill."
-fi
-docker ps -a | grep container_lms_course_service && docker rm container_lms_course_service
+#docker ps -a | grep container_lms_course_service && docker rm container_lms_course_service
