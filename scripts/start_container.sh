5b2c1d5f3827e1a5b1250612daa66b40f7fb1375
DamThanhLam <121682538+DamThanhLam@users.noreply.github.com>
2025-02-06 01:15:31 +0700

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,9 +1,7 @@
 
 set -e
 cd "$(dirname "$0")/.."
-sudo chown -R root:root ~/aws.env
-sudo chmod -R 755 ~/aws.env
 
 docker build -t image_lms_course_service:latest course_service/
-sudo docker run -d -v /home/ubuntu/aws.env:/root  -p 8080:8080 --name container_lms_course_service image_lms_course_service:latest
+docker run -d -v /home/ubuntu/aws.env:/root  -p 8080:8080 --name container_lms_course_service image_lms_course_service:latest
 
