63c1fb9c684eb5260c62b50375f42ec8d92fdbc1
DamThanhLam <121682538+DamThanhLam@users.noreply.github.com>
2025-02-05 20:02:06 +0700

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,10 +1,10 @@
 #set -e
-#docker stop container_lms_course_service
-#if docker ps -a | grep -q "container_lms_course_service"; then
-#    echo "Stopping container_lms_course_service..."
-#    docker stop container_lms_course_service
-#    docker rm container_lms_course_service
-#else
-#    echo "Container container_lms_course_service not found, nothing to stop."
-#fi
-docker ps -a | grep container_lms_course_service && docker rm container_lms_course_service
+docker stop container_lms_course_service
+if docker ps -a | grep -q "container_lms_course_service"; then
+    echo "Stopping container_lms_course_service..."
+    docker stop container_lms_course_service
+    docker rm container_lms_course_service
+else
+    echo "Container container_lms_course_service not found, nothing to stop."
+fi
+#docker ps -a | grep container_lms_course_service && docker rm container_lms_course_service
