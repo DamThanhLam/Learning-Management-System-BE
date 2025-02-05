dad095cc5f7f547f3143a187d72e6a7d10376316
DamThanhLam <121682538+DamThanhLam@users.noreply.github.com>
2025-02-05 22:50:07 +0700

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,1 +1,23 @@
 
+#!/bin/bash
+
+# Kiểm tra nếu có container nào
+container_ids=$(docker ps -a -q)
+
+if [ -z "$container_ids" ]; then
+  echo "Không có container nào để dừng hoặc xóa."
+else
+  # Kiểm tra tất cả các container đang chạy
+  echo "Danh sách tất cả các container đang chạy:"
+  docker ps -a
+
+  # Dừng tất cả các container đang chạy
+  echo "Đang dừng tất cả các container..."
+  docker stop $container_ids
+
+  # Xóa tất cả các container đã dừng
+  echo "Đang xóa tất cả các container..."
+  docker rm $container_ids
+
+  echo "Đã dừng và xóa tất cả các container."
+fi
