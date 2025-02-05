c3529e35225025610821aefade75fddc0847226b
DamThanhLam <121682538+DamThanhLam@users.noreply.github.com>
2025-02-05 23:30:03 +0700

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,5 +5,5 @@ sudo chown -R root:root ~/.aws
 sudo chmod -R 755 ~/.aws
 
 docker build -t image_lms_course_service:latest course_service/
-docker run -d -p 8080:8080 -v ~/.aws:/root/.aws --name container_lms_course_service image_lms_course_service:latest
+docker run -d -p 8080:8080 -v ~/.aws:/root --name container_lms_course_service image_lms_course_service:latest
 
