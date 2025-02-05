5bec39c449c209462b35a157792c29228bc5d63b
DamThanhLam <121682538+DamThanhLam@users.noreply.github.com>
2025-02-05 20:03:58 +0700

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,10 +1,16 @@
-#set -e
-docker stop container_lms_course_service
+#!/bin/bash
+
+# Kiểm tra nếu container đang chạy hoặc đã dừng
 if docker ps -a | grep -q "container_lms_course_service"; then
     echo "Stopping container_lms_course_service..."
-    docker stop container_lms_course_service
+    
+    # Nếu container đang chạy, dừng nó
+    if docker ps | grep -q "container_lms_course_service"; then
+        docker stop container_lms_course_service
+    fi
+    
+    # Xóa container
     docker rm container_lms_course_service
 else
     echo "Container container_lms_course_service not found, nothing to stop."
 fi
-#docker ps -a | grep container_lms_course_service && docker rm container_lms_course_service
