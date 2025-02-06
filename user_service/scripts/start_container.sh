9fc14a34684a4ab3cccd9ae03802d42d7eb7c4f7
DamThanhLam <121682538+DamThanhLam@users.noreply.github.com>
2025-02-07 00:30:59 +0700

    Update start_container.sh

--- user_service/scripts/start_container.sh
+++ user_service/scripts/start_container.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")/.."
 export PATH=$PATH:/home/ubuntu/.sdkman/candidates/gradle/current/bin
 gradle build -x test
 
-docker build -t image_lms_user_service:latest .
-docker run -d -v /home/ubuntu/aws.env:/root/aws.env -p 8080:8080 --name container_lms_user_service image_lms_user_service:latest
+sudo docker build -t image_lms_user_service:latest .
+sudo docker run -d -v /home/ubuntu/aws.env:/root/aws.env -p 8080:8080 --name container_lms_user_service image_lms_user_service:latest
 
