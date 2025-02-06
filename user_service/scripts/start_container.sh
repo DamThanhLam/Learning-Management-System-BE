cb34d4f319250538416d27add2a5acb64feb8cb9
DamThanhLam <121682538+DamThanhLam@users.noreply.github.com>
2025-02-06 21:51:34 +0700

    Update start_container.sh

--- user_service/scripts/start_container.sh
+++ user_service/scripts/start_container.sh
@@ -2,6 +2,7 @@
 set -e
 cd "$(dirname "$0")/.."
 
+gradle build -x test
 
 docker build -t image_lms_user_service:latest .
 docker run -d -v /home/ubuntu/aws.env:/root/aws.env -p 8080:8080 --name container_lms_user_service image_lms_user_service:latest
