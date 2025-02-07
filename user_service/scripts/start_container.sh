e04c86568acf850f81a7f2cf6c81715a72dabe56
DamThanhLam <121682538+DamThanhLam@users.noreply.github.com>
2025-02-07 11:36:26 +0700

    Update start_container.sh

--- user_service/scripts/start_container.sh
+++ user_service/scripts/start_container.sh
@@ -4,6 +4,7 @@ cd "$(dirname "$0")/.."
 
 export PATH=$PATH:/home/ubuntu/.sdkman/candidates/gradle/current/bin
 gradle build -x test
+aws appconfig get-configuration --application "application-config-lms-app" --environment "dev" --configuration "lms-app" --client-id "test2" src/main/resources/application.yaml
 
 sudo docker build -t image_lms_user_service:latest .
 sudo docker run -d --env-file /home/ubuntu/aws.env -p 8080:8080 --name container_lms_user_service image_lms_user_service:latest
