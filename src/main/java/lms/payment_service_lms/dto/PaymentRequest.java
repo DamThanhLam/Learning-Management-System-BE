4723984be8962b36992f0cf7ca88bd4d8c005eaa
DaoNgocAnhKhoi <daongocanhkhoi@gmail.com>
2025-04-27 10:42:43 +0700

    update model

--- src/main/java/lms/payment_service_lms/dto/PaymentRequest.java
+++ src/main/java/lms/payment_service_lms/dto/PaymentRequest.java
@@ -1,13 +1,17 @@
 package lms.payment_service_lms.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PaymentRequest {
     String userId;
